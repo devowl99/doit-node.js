9b83552df08353a698958a483ddecfd0c792d49e
Shihyun Han <devowl99@gmail.com>
2024-04-04 21:15:42 +0900

    enter

--- myContacts/controllers/contactController.js
+++ myContacts/controllers/contactController.js
@@ -1,21 +1,72 @@
+const asyncHandeler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
 // Get all contacts
 // GET /contacts
-
-const asyncHandeler = require("express-async-handler");
-
 const getAllContacts = asyncHandeler(async(req,res)=>{
-    res.send("Contacts Page");
+    const contacts = await Contact.find();
+    res.send(contacts);
 });
 
+// Create contact
+// POST /contacts
 const createContact = asyncHandeler(async(req,res)=>{
     console.log(req.body);
     const {name, email, phone} = req.body;
     if(!name || !email || !phone){
         return res.send("필수 값이 입력되지 않았습니다.");
     }
+    const contact = await Contact.create({
+        name, email, phone
+    });
     res.send("Create Contacts");
 });
 
+// Get contact
+// GET /contacts/:id
+const getContact = asyncHandeler(async(req,res)=>{
+    const contact = await Contact.findById(req.params.id);
+    res.send(contact);
+});
+
+// Update contact
+// PUT /contacts/:id
+const updateContact = asyncHandeler(async(req,res)=>{
+    const id = req.params.id;
+    const {name, email, phone} = req.body;
+    const contact = await Contact.findById(id);
+    if (!contact){
+        throw new Error("Contact not found.");
+    }
+
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+
+    contact.save();
+
+    res.json(contact);
+})
+
+// Delete contact
+// DELETE /contacts/:id
+const deleteContact = asyncHandeler(async(req,res)=>{
+    const id = req.params.id;
+
+    const contact = await Contact.findById(id);
+    if (!contact){
+        throw new Error("Contact not found.");
+    }
+    // 여기까진 update와 거의 동일
+
+    await Contact.deleteOne();
+    res.send("Deleted");
+})
+
 module.exports = {
-    getAllContacts, createContact
+    getAllContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
 };
--- myContacts/routes/contactRoutes.js
+++ myContacts/routes/contactRoutes.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {getAllContacts, createContact} = require("../controllers/contactController");
+const {getAllContacts, createContact, getContact, updateContact, deleteContact}
+= require("../controllers/contactController");
 
 router.route("/")
     .get(getAllContacts)
     .post(createContact);
 
 router.route("/:id")
-    .get((req,res)=>{
-        res.send(`View Contact for ID : ${req.params.id}`);
-    })
-    .put((req,res)=>{
-        res.send(`Update Contact for ID : ${req.params.id}`);
-    })
-    .delete((req,res)=>{
-        res.send(`Delete Contact for ID : ${req.params.id}`);
-    });
+    .get(getContact)
+    .put(updateContact)
+    .delete(deleteContact);
 
 module.exports = router;
