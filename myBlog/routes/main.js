d7aa960a34774834801658f0e8563134b7b109f1
Shihyun Han <devowl99@gmail.com>
2024-04-12 04:31:00 +0900

    enter

---
+++ myBlog/routes/main.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs"; // layout 경로
+
+router.get(["/", "/home"], (req,res) => {
+    const locals = {
+        title: "Home"
+    }
+    res.render("index", {locals, layout: mainLayout})
+})
+
+router.get("/about", (req,res) => {
+    const locals = {
+        title: "About"
+    }
+    res.render("about", {locals, layout: mainLayout})
+})
+
+module.exports = router;
