865af95e1ae79f93fd90710fd12f5e96e341272a
Shihyun Han <devowl99@gmail.com>
2024-04-12 06:56:52 +0900

    enter

--- myBlog/routes/main.js
+++ myBlog/routes/main.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const mainLayout = "../views/layouts/main.ejs"; // layout 경로
+const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asyncHandler = require("express-async-handler"); // 비동기 처리 위해 asyncHandler 가져옴
 
-router.get(["/", "/home"], (req,res) => {
-    const locals = {
-        title: "Home"
-    }
-    res.render("index", {locals, layout: mainLayout})
-})
+router.get(
+    ["/", "/home"],
+    asyncHandler(async (req,res) => {
+        const locals = {
+            title: "Home"
+        }
+        const data = await Post.find(); // db의 모든 게시물 가져옴
+        res.render("index", {locals, data, layout: mainLayout}) // 가져온 값을 index.ejs로 넘겨줌
+    })
+)
 
 router.get("/about", (req,res) => {
     const locals = {
@@ -15,5 +21,15 @@ router.get("/about", (req,res) => {
     }
     res.render("about", {locals, layout: mainLayout})
 })
+
+// 게시물 상세 보기
+// GET /post/:id
+router.get(
+    "/post/:id",
+    asyncHandler(async(req,res)=>{
+        const data = await Post.findOne({_id: req.params.id});
+        res.render("post", {data, layout: mainLayout});
+    })
+)
 
 module.exports = router;
